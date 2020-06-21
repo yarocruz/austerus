89d08e79bb213fafabce29aadd31dd5c9a1ecadf
yarocruz <yarocruz@gmail.com>
2020-06-21 13:18:51 -0400

    adding example template code

--- app/public/wp-content/themes/austerus/page.php
+++ app/public/wp-content/themes/austerus/page.php
@@ -1,10 +1,14 @@
 <?php
 
-while(have_posts()) { // have_posts() finds the posts that are created from dashboard, which are saved in the database
-    the_post(); ?>
-    <h1>This is a page not a post</h1>
-    <h2><?php the_title(); ?></h2>
-    <?php the_content(); ?>
-<?php }
+    get_header();
+
+    while(have_posts()) { // have_posts() finds the posts that are created from dashboard, which are saved in the database
+        the_post(); ?>
+        <h1>This is a page not a post</h1>
+        <h2><?php the_title(); ?></h2>
+        <?php the_content(); ?>
+    <?php }
+
+    get_footer();
 
 ?>
--- app/public/wp-content/themes/austerus/single.php
+++ app/public/wp-content/themes/austerus/single.php
@@ -1,9 +1,13 @@
 <?php
 
-while(have_posts()) { // have_posts() finds the posts that are created from dashboard, which are saved in the database
-    the_post(); ?>
-    <h2><?php the_title(); ?></h2>
-    <?php the_content(); ?>
-<?php }
+    get_header();
+
+    while(have_posts()) { // have_posts() finds the posts that are created from dashboard, which are saved in the database
+        the_post(); ?>
+        <h2><?php the_title(); ?></h2>
+        <?php the_content(); ?>
+    <?php }
+
+    get_footer();
 
 ?>
