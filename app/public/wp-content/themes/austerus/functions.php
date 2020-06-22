40e381a21e7b453e07c55d26ca5a63d7df23684e
yarocruz <yarocruz@gmail.com>
2020-06-22 15:29:46 -0400

    archived past events, added custom queries for them, etc

--- app/public/wp-content/themes/austerus/functions.php
+++ app/public/wp-content/themes/austerus/functions.php
@@ -18,3 +18,24 @@
 
     add_action('after_setup_theme', 'university_features');
 
+    function university_adjust_queries($query) {
+        if (!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()) {
+            $today = date('Ymd');
+            $query->set('meta_key', 'event_date');
+            $query->set('orderby', 'meta_value_num');
+            $query->set('order', 'ASC');
+            $query->set('meta_query', array(
+                array(
+                    'key' => 'event_date',
+                    'compare' => '>=',
+                    'value' => $today,
+                    'type' => 'numeric'
+                )
+            ));
+        }
+    }
+
+    add_action('pre_get_posts', 'university_adjust_queries');
+
+
+
