a0ee9b800487c6a6989b2d47e4483b541b42e784
yarocruz <yarocruz@gmail.com>
2020-06-22 16:19:11 -0400

    events now have a relationship to programs

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -1,6 +1,7 @@
 <?php
 
 function university_post_types() {
+    // Even Post Type
     register_post_type('event', array(
     'show_in_rest' => true,
     'supports' => array('title', 'editor', 'excerpt'),
@@ -15,6 +16,23 @@ function university_post_types() {
     'singular_name' => 'Event'
     ),
     'menu_icon' => 'dashicons-calendar',
+    ));
+
+    // Program Post Type
+    register_post_type('program', array(
+        'show_in_rest' => true,
+        'supports' => array('title', 'editor'),
+        'rewrite' => array('slug' => 'programs'),
+        'has_archive' => true,
+        'public' => true,
+        'labels' => array(
+            'name' => 'Programs',
+            'add_new_item' => 'Add New Program',
+            'edit_item' => 'Edit Program',
+            'all_items' =>  'All Programs',
+            'singular_name' => 'Program'
+        ),
+        'menu_icon' => 'dashicons-awards',
     ));
 }
 
--- app/public/wp-content/themes/austerus/single-event.php
+++ app/public/wp-content/themes/austerus/single-event.php
@@ -23,6 +23,22 @@ while(have_posts()) { // have_posts() finds the posts that are created from dash
         <div class="generic-content">
             <?php the_content();?>
         </div>
+
+        <?php
+            $relatedPrograms = get_field('relate_programs');
+
+            if ($relatedPrograms) {
+                echo '<hr class="section-break">';
+                echo '<h2 class="headline headline--medium">Related Program(s)</h2>';
+                echo '<ul class="link-list min-list">';
+                foreach ($relatedPrograms as $program) { ?>
+                    <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
+                <?php }
+                echo '</ul>';
+            }
+
+
+        ?>
     </div>
 <?php }
 
