cbd280550b221999a4912a7745460b09b2f40641
yarocruz <yarocruz@gmail.com>
2020-06-23 09:51:05 -0400

    finished with the automation dev setup

--- app/public/wp-content/themes/austerus/functions.php
+++ app/public/wp-content/themes/austerus/functions.php
@@ -1,10 +1,17 @@
 <?php
     // creates and defines a function to be able to use the custom css, js, fonts
     function austere_files() {
-        wp_enqueue_script('main-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true);
         wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
         wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-        wp_enqueue_style('austere_main_styles', get_stylesheet_uri());
+
+        if (strstr($_SERVER['SERVER_NAME'], 'austerus.local')) {
+            wp_enqueue_script('main-js', 'http://localhost:3000/bundled.js', NULL, microtime(), true);
+        } else {
+            wp_enqueue_script('vendors-js', get_theme_file_uri('/bundled-assets-vendors.js'), NULL, microtime(), true);
+            wp_enqueue_script('main-js', get_theme_file_uri('/bundled-assets-scripts.js'), NULL, microtime(), true);
+            wp_enqueue_style('main-styles', get_theme_file_uri('/bundled-assets/style.css'));
+        }
+
     }
 
     add_action('wp_enqueue_scripts', 'austere_files');
