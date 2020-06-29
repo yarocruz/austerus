685739b7e23b32c4eb15ab3441d17add527153e4
yarocruz <yarocruz@gmail.com>
2020-06-29 19:54:24 -0400

    deployed website

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -70,22 +70,37 @@ function university_post_types() {
         'menu_icon' => 'dashicons-welcome-learn-more',
     ));
 
-    // Note Post Type
-    register_post_type('note', array(
-        'capability_type' => 'note',
-        'map_meta_cap' => true,
-        'show_in_rest' => true,
-        'supports' => array('title', 'editor'),
+//    // Note Post Type
+//    register_post_type('note', array(
+//        'capability_type' => 'note',
+//        'map_meta_cap' => true,
+//        'show_in_rest' => true,
+//        'supports' => array('title', 'editor'),
+//        'public' => false,
+//        'show_ui' => true,
+//        'labels' => array(
+//            'name' => 'Notes',
+//            'add_new_item' => 'Add New Note',
+//            'edit_item' => 'Edit Note',
+//            'all_items' =>  'All Notes',
+//            'singular_name' => 'Note'
+//        ),
+//        'menu_icon' => 'dashicons-welcome-write-blog',
+//    ));
+
+    // Like Post Type
+    register_post_type('like', array(
+        'supports' => array('title'),
         'public' => false,
         'show_ui' => true,
         'labels' => array(
-            'name' => 'Notes',
-            'add_new_item' => 'Add New Note',
-            'edit_item' => 'Edit Note',
-            'all_items' =>  'All Notes',
-            'singular_name' => 'Note'
+            'name' => 'Likes',
+            'add_new_item' => 'Add New Like',
+            'edit_item' => 'Edit Like',
+            'all_items' =>  'All Likes',
+            'singular_name' => 'Like'
         ),
-        'menu_icon' => 'dashicons-welcome-write-blog',
+        'menu_icon' => 'dashicons-heart',
     ));
 }
 
