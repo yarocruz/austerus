dab83c5e338ab8eaf64b2b3fcd0fa648a5c8f9ff
yarocruz <yarocruz@gmail.com>
2020-06-30 14:46:53 -0400

    deleting fictional-university specific theme files

--- app/public/wp-content/themes/austerus/searchform.php
+++
@@ -1,7 +0,0 @@
-<form class="search-form" method="get" action="<?php echo esc_url(site_url('/')); ?>">
-    <label class="headline headline--medium" for="s">Perform a new Search:</label>
-    <div class="search-form-row">
-        <input placeholder="What are you looking for?" class="s" id="search" type="search" name="s">
-        <input class="search-submit" type="submit" value="Search">
-    </div>
-</form>
